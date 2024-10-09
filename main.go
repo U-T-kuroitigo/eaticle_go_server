220d637193608c82952bfdc2c91fddd4f4608742
U-T-kuroitigo <git.cq.kx@gmail.com>
2024-10-10 00:47:52 +0900

    fix/main.go
    修正・変更を忘れていたmain.goを修正

--- main.go
+++ main.go
@@ -3,12 +3,20 @@ package main
 import (
 	"fmt"
 
-	"github.com/U-T-kuroitigo/eaticle/routes"
+	"github.com/U-T-kuroitigo/eaticle_go_server/configuration"
+	"github.com/U-T-kuroitigo/eaticle_go_server/routes"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
 )
 
 func main() {
+	// データベースの初期化
+	db := configuration.InitDB()
+	defer func() {
+		sqlDB, _ := db.DB()
+		sqlDB.Close()
+	}()
+
 	e := echo.New()
 	e.Use(middleware.Recover())
 	e.Use(middleware.Logger())
